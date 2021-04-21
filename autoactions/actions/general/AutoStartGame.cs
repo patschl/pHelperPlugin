f2a062ceda5d3c0e538534922313f6d65487b33d
s4000-hk <82436438+s4000-hk@users.noreply.github.com>
2021-04-21 12:46:46 +0800

    Update AutoStartGame.cs
    revise for able to input the text on start game button for non english user

--- autoactions/actions/general/AutoStartGame.cs
+++ autoactions/actions/general/AutoStartGame.cs
@@ -1,37 +1,42 @@
-﻿namespace Turbo.plugins.patrick.autoactions.actions.town
+namespace Turbo.plugins.patrick.autoactions.actions.town
 {
-    using System.Collections.Generic;
+	using System.Collections.Generic;
 	using System.Text;
-    using parameters;
-    using Plugins;
-    using util.diablo;
-    using util.thud;
+	using parameters;
+	using parameters.types;
+	using Plugins;
+	using util.diablo;
+	using util.thud;
 
-    public class AutoStartGame : AbstractAutoAction
-    {
-        public override string tooltip => "Automatically starts game in the lobby.";
+	public class AutoStartGame : AbstractAutoAction
+	{
+		public override string tooltip => "Automatically starts game in the lobby.";
 
-        public override string GetAttributes() => "";
+		public override string GetAttributes() => $"[ Text On Game Start Button: {TextOnButton} ]";
 
-        public override long minimumExecutionDelta => 500;
+		public override long minimumExecutionDelta => 500;
 
-        public override List<AbstractParameter> GetParameters()
-        {
-            return new List<AbstractParameter>();
-        }
+		public string TextOnButton { get; set; } = "Start";
 
-        public override bool Applicable(IController hud)
-        {
-            return !hud.Game.IsInGame 
-            && !hud.Game.IsLoading
-            && hud.Render.IsUiElementVisible(UiPathConstants.Buttons.START_GAME)
-            && hud.Render.GetUiElement(UiPathConstants.Buttons.START_GAME)
-                  .ReadText(Encoding.ASCII, true).Contains("Start");
-        }
+		public override List<AbstractParameter> GetParameters()
+		{
+			return new List<AbstractParameter> {
+				SimpleParameter<string>.of(nameof(TextOnButton), x => TextOnButton = x)
+			};
+		}
 
-        public override void Invoke(IController hud)
-        {
-            hud.Render.WaitForVisiblityAndClickOrAbortHotkeyEvent(UiPathConstants.Buttons.START_GAME);
-        }
-    }
+		public override bool Applicable(IController hud)
+		{
+			return !hud.Game.IsInGame
+			&& !hud.Game.IsLoading
+			&& hud.Render.IsUiElementVisible(UiPathConstants.Buttons.START_GAME)
+			&& hud.Render.GetUiElement(UiPathConstants.Buttons.START_GAME)
+				  .ReadText(Encoding.UTF8, true).Contains(TextOnButton);
+		}
+
+		public override void Invoke(IController hud)
+		{
+			hud.Render.WaitForVisiblityAndClickOrAbortHotkeyEvent(UiPathConstants.Buttons.START_GAME);
+		}
+	}
 }
